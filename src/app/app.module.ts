f17a70136c8c224ec686ef508b604f772eaf322f
francofrighetto <68360116+francofrighetto@users.noreply.github.com>
2024-08-31 16:20:40 -0300

    actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 // import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
 import { BtnSubirComponent } from './components/btn-subir/btn-subir.component';
 import { CardProjectComponent } from './components/card-project/card-project.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 import { PresentacionComponent } from './components/presentacion/presentacion.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
-import { HeaderComponent } from './components/header/header.component';
 import { SliderClientesComponent } from './components/slider-clientes/slider-clientes.component';
 import { VerMasComponent } from './components/ver-mas/ver-mas.component';
-import { AppRoutingModule } from './app-routing.module';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
