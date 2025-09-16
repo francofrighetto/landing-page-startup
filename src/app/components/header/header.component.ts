e08dc8152ea547e7b1433c3b04ff72319cdf5e7e
franco frighetto <frighetto@xcapit.com>
2025-09-16 18:42:56 -0300

    avance

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { DatosPortfolioService } from 'src/app/services/datos-portfolio.service';
 import { Persona } from 'src/app/Modelo/Persona';
 import { PersonabdService } from 'src/app/personabd.service';
+import { DatosJsonService } from 'src/app/services/datos-json.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  // llamo a un servicio (haces datosPortfolio:DatosPortfolioService es como hacer String cadena (var y tipo, referencia))
   constructor(
     private service: PersonabdService,
-    private serviceJSON: DatosPortfolioService
-  ) {}
+    private serviceJSON: DatosJsonService
+  ) { }
 
   header: any;
   headerJSON: any;
